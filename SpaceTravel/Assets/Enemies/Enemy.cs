26e767e2cf4df3893c9669699c5d742fe4870cdb
Roman <Roman@VATO>
2017-08-15 15:50:16 +0400

    Добавлен класс Enemy

--- SpaceTravel/Assets/Enemies/Enemy.cs
+++ SpaceTravel/Assets/Enemies/Enemy.cs
@@ -10,23 +10,35 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Microsoft.Xna.Framework.Graphics;
+using Android.Graphics;
 
 namespace SpaceTravel.Assets.Enemies
 {
     public class Enemy
     {
+        public Point Position { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
         public int Health { get; set; }
         public int Damage { get; set; }
         public int Speed  { get; set; }
         public int Armor  { get; set; } // Броня
         //public int Abilities { get; set; } //Способности
         public Texture2D TextureEnemy { get; set; }
-        public Enemy(int health, int damage,int speed, int armor)
+        public Enemy(int health, int damage,int speed, int armor,Point position, int height, int width )
         {
             Health = health;
             Damage = damage;
             Speed = speed;
             Armor = armor;
+            Position = position;
+            Height = height;
+            Width = width;
+
+        }
+        public virtual void Move ()
+        {
+
         }
     }
 
